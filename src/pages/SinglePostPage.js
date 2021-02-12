36405e6ec0c992b451ff9e67bc6c5f7e96acce82
giacomo9999 <gmarchesi888@gmail.com>
2021-02-12 17:16:54 -0500

    Everything working--weird reload issue persists.

--- src/pages/SinglePostPage.js
+++ src/pages/SinglePostPage.js
@@ -3,17 +3,39 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 import { fetchSinglePost } from "../actions/singlePostActions";
+import { fetchComments } from "../actions/commentsActions";
 
-const SinglePostPage = ({ match, dispatch, postData, loading, hasError }) => {
+const SinglePostPage = ({
+  match,
+  dispatch,
+  postData,
+  commentData,
+  loading,
+  hasError,
+}) => {
   useEffect(() => {
     const { postId } = match.params;
     dispatch(fetchSinglePost(postId));
+    dispatch(fetchComments(postId));
   }, [dispatch, match]);
+
+  const renderComments = () => {
+    console.log("Comments Data", commentData);
+    if (loading.comments) return <p>Loading Posts...</p>;
+    if (hasError.comments) return <p>An Error Occurred</p>;
+    return commentData.map((comment) => (
+      <aside className="comment">
+        <h2>{comment.title}</h2>
+        <h3>{comment.email}</h3>
+        <p>{comment.body}</p>
+      </aside>
+    ));
+  };
 
   const renderPost = () => {
     console.log("Post Data: ", postData);
-    if (loading) return <p>Loading Posts...</p>;
-    if (hasError) return <p>An Error Occurred</p>;
+    if (loading.singlePost) return <p>Loading Posts...</p>;
+    if (hasError.singlePost) return <p>An Error Occurred</p>;
     return (
       <section>
         {/* <h1>POST</h1> */}
@@ -24,13 +46,25 @@ const SinglePostPage = ({ match, dispatch, postData, loading, hasError }) => {
       </section>
     );
   };
-  return renderPost();
+  return (
+    <section>
+      {renderPost()}
+      {renderComments()}
+    </section>
+  );
 };
 
 const mapStateToProps = (state) => ({
-  loading: state.singlePost.loading,
+  commentData: state.comments.commentData,
   postData: state.singlePost.postData,
-  hasError: state.singlePost.hasError,
+  loading: {
+    singlePost: state.singlePost.loading,
+    comments: state.comments.loading,
+  },
+  hasError: {
+    singlePost: state.singlePost.hasError,
+    comments: state.comments.hasError,
+  },
 });
 
 export default connect(mapStateToProps)(SinglePostPage);
